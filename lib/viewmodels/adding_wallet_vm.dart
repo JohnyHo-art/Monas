f0eb7c1020ec465118562b16ff96dfdac7795178
leductrung2310 <19522422@gm.uit.edu.vn>
2022-05-10 14:38:17 +0700

    update merge

--- lib/viewmodels/adding_wallet_vm.dart
+++ lib/viewmodels/adding_wallet_vm.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:monas/constants/constants.dart';
 import 'package:monas/constants/resources.dart';
 import 'package:monas/constants/utils.dart';
