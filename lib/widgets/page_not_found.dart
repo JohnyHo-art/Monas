7330b7f3f0f12abcb171309b9aa9f8b1fbf54475
JohnyHo-art <71172797+JohnyHo-art@users.noreply.github.com>
2022-06-21 01:15:41 +0700

    new update

--- lib/widgets/page_not_found.dart
+++ lib/widgets/page_not_found.dart
@@ -10,19 +10,25 @@ class PageNotFound extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: S.colors.whiteColor,
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(R.illutrations.pageNotFound),
-              SizedBox(height: S.dimens.padding),
-              Text(
-                'Tính năng đang phát triển\nVui lòng trở lại sau',
-                style: S.bodyTextStyles.body1(S.colors.subTextColor2),
-                textAlign: TextAlign.center,
-              ),
-            ],
+        appBar: AppBar(
+          backgroundColor: S.colors.whiteColor,
+          elevation: 0,
+          title: Text(
+            'Báo cáo',
+            style: S.headerTextStyles.appbarTitle(null),
           ),
+        ),
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(R.illutrations.pageNotFound),
+            SizedBox(height: S.dimens.padding),
+            Text(
+              'Tính năng đang phát triển\nVui lòng trở lại sau',
+              style: S.bodyTextStyles.body1(S.colors.subTextColor2),
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
     );
